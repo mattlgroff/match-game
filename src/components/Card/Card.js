a8d45993a544e3669902764c4f15ebdddf9fbb49
Matt Groff <mattlgroff@gmail.com>
2017-12-16 09:25:55 -0800

    separated pokeball and pokemon classes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -16,15 +16,17 @@ export default class Card extends Component {
   render() {
     // let cardImg = `https://github.com/mattlgroff/match-game/blob/gh-pages/assets/images/pokeball.png`
     let cardImg = "./assets/images/pokeball.png";
+    let cardClass = "pokeball";
 
     if(this.props.flipped){
       // cardImg = `https://github.com/mattlgroff/match-game/blob/gh-pages/assets/images/${this.props.value}.gif`
+      cardClass = "pokemon";
       cardImg = `./assets/images/${this.props.value}.gif`;
     } 
 
     return (
       <div className="col-sm-6 col-xs-6 col-md-6 col-lg-6 cardContainer text-center">
-        <img className="pokemon" alt="card" onClick={this.handleClick} src={cardImg} />
+        <img className={cardClass} alt="card" onClick={this.handleClick} src={cardImg} />
       </div>  
     );
   }
